8af42dbbeb7db21c36c0b49883ae38f95d22d4c2
KevinBaoB <kevinbaobui@gmail.com>
2022-05-25 20:17:13 -0500

    First attempt

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,19 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
+// exports.findArmstrongNumbers =
 
-};
+function findArmstrongNumbers() {
+  let list = [];
+  for (let i = 0; i < 999; i++) {
+    num = String(i).split("");
+    let sum = 0;
+    for (let i in num) {
+      sum += Math.pow(+num[i], num.length);
+    }
+    if (sum === i) {
+      list.push(sum);
+    }
+  }
+  return list;
+}
+console.log(findArmstrongNumbers());
