45bc17d609482fb8f74e719d70728f914d6b4c19
KevinBaoB <kevinbaobui@gmail.com>
2022-05-26 15:53:54 -0500

    First

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,16 @@
 # How can you make this more scalable and reusable later?
+from code import interact
+
+
 def find_armstrong_numbers(numbers):
+    finalArray = []
+    for i in range(len(numbers)):
+        num = list(str(numbers[i]))
+        sum = 0
+        for j in num:
+            sum += pow(int(j), len(num))
+        if sum == int(i):
+            finalArray.append(sum)
+    return finalArray
+
+# print(find_armstrong_numbers(list(range(0,999))))
